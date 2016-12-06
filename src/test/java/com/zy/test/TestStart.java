6269f479e7a4f307484e69ad2d77f8bc6e915f54
zhangyu <zhangyu@kakasure.com>
2016-12-06 14:25:32 +0800

    第一次提交commit hello world

---
+++ src/test/java/com/zy/test/TestStart.java
@@ -0,0 +1,11 @@
+
+package com.zy.test;
+
+import java.util.Date;
+
+public class TestStart {
+
+	public static void main(String[] args) {
+		System.err.println("zy-test-resent Start " + new Date());
+	}
+}
