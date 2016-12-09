fcc7ed9ea33159d64ceb73edcf03d3ee48e79d01
zhangyu <zhangyu@kakasure.com>
2016-12-09 09:40:38 +0800

    delete 📛

--- src/test/java/com/zy/test/TestStart.java
+++ src/test/java/com/zy/test/TestStart.java
@@ -8,5 +8,7 @@ public class TestStart {
 	public static void main(String[] args) {
 		System.err.println("zy-test-resent Start " + new Date());
 		System.err.println("zy-test-resent Start aaa" + new Date());
+		System.err.println("zy-test-resent Start aaa2" + new Date());
+		System.err.println("zy-test-resent Start aaa3" + new Date());
 	}
 }
